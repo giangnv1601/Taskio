60c1f72679fdca20480716f0c3e475f73427411a
Nguyen Van Giang <giang16012405@gmail.com>
2025-10-19 04:22:58 +0700

    feat(ui): implement dark and light mode theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,27 @@ import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import { Typography } from '@mui/material'
 
+import { useColorScheme } from '@mui/material/styles';
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
+
 function App() {
 
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>giangnv</div>
 
       <Typography variant="body2" color="text.secondary">Test Typo</Typography>
